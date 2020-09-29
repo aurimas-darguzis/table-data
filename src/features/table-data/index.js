a052b9fadc997d52158ef03f0ee2804f971cdf89
aurimasdarguzis <aurimasdarguzis@gmail.com>
2020-09-29 17:31:26 +0100

    feat: paggination support

--- src/features/table-data/index.js
+++ src/features/table-data/index.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
-import data from "../../MOCK_DATA.json";
-import { TableDataProvider } from "./context";
 import useTableData from "./hooks";
+import "./tableData.css";
 
 export default function TableData() {
-  const [itemsPerPage, setItemsPerPage] = useState(null);
-
   const { data } = useTableData();
-  console.log("data from state: ", data);
+  const [pageStartPosition, setPageStartPosition] = useState(0);
+  const [pageEndPosition, setPageEndPosition] = useState(20);
+  const [itemsPerPage, setItemsPerPage] = useState(20);
 
   function sortByKey(key) {
     return;
@@ -17,14 +16,99 @@ export default function TableData() {
     return;
   }
 
+  function onNext() {
+    const endPosition =
+      pageEndPosition + itemsPerPage > data.length
+        ? data.length
+        : pageEndPosition + itemsPerPage;
+    const startPosition =
+      endPosition === data.length
+        ? data.length - itemsPerPage
+        : pageEndPosition;
+    setPageStartPosition(startPosition);
+    setPageEndPosition(endPosition);
+  }
+
+  function onPrev() {
+    const startPosition =
+      pageStartPosition - itemsPerPage > 0
+        ? pageStartPosition - itemsPerPage
+        : 0;
+    const endPosition = startPosition === 0 ? itemsPerPage : pageStartPosition;
+
+    setPageStartPosition(startPosition);
+    setPageEndPosition(endPosition);
+  }
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>labas</th>
-        </tr>
-      </thead>
-      <tbody></tbody>
-    </table>
+    <>
+      <label>Items per page:</label>
+      <select onChange={(e) => setItemsPerPage(Number(e.target.value))}>
+        <option>20</option>
+        <option>50</option>
+        <option>150</option>
+        <option>200</option>
+      </select>
+      <button disabled={pageStartPosition === 0} onClick={onPrev}>
+        Previous
+      </button>
+      <button disabled={pageEndPosition === data.length} onClick={onNext}>
+        Next
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>
+              Date of birth
+              <span role='img' aria-label='sort ascending'>
+                🔽
+              </span>
+              <span role='img' aria-label='sort desceding'>
+                🔼
+              </span>
+            </th>
+            <th>Email</th>
+            <th>
+              Salary
+              <span role='img' aria-label='sort ascending'>
+                🔽
+              </span>
+              <span role='img' aria-label='sort desceding'>
+                🔼
+              </span>
+            </th>
+            <th>Years of experience</th>
+            <th>
+              <span role='img' aria-label='sort ascending'>
+                🔽
+              </span>
+              <span role='img' aria-label='sort desceding'>
+                🔼
+              </span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.slice(pageStartPosition, pageEndPosition).map((d) => {
+              return (
+                <tr key={d.id}>
+                  <td>{d.id}</td>
+                  <td>{d.first_name}</td>
+                  <td>{d.last_name}</td>
+                  <td>{d.date_of_birth} </td>
+                  <td>{d.email}</td>
+                  <td>{d.salary}</td>
+                  <td>{d.years_of_experience}</td>
+                  <td>{d.industry}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </>
   );
 }
